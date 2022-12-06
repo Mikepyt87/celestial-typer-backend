14b519c6f00c4640ec48f3a00c328031d6f1da76
Mikepyt87 <81245551+Mikepyt87@users.noreply.github.com>
2022-12-06 11:56:35 -0500

    already pushed real good but this is pushed better

---
+++ functions/src/models/Article.ts
@@ -0,0 +1,11 @@
+// Interface describes the return from the endpoint from Splace Flight Api
+export default interface Article {
+  id: number;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite: string;
+  summary: string;
+  publishedAt: string;
+  updatedAt: string;
+}
--- functions/src/models/Typer.ts
+++ functions/src/models/Typer.ts
@@ -1,5 +1,11 @@
 import { ObjectId } from "mongodb";
+import Article from "./Article";
+import Score from "./Score";
 
 export default interface Typer {
   _id?: ObjectId;
+  profilePic: string;
+  userName: string;
+  scores: Score[];
+  favoritedArticles: Article[];
 }
